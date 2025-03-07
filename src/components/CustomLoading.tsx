d253f30030d3bac1caf9ffaf5bf3163b9f6b0060
kronberh <10252213@stud.op.edu.ua>
2025-03-07 21:19:27 +0200

    pagination + loading indicator + fixed infiite fetch loop

---
+++ src/components/CustomLoading.tsx
@@ -0,0 +1,63 @@
+export default function CustomLoading() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid"
+            width="300"
+            height="300"
+            style={{ shapeRendering: "auto", display: "block", margin: "auto" }}
+        >
+            <g>
+                <rect fill="#f9253f" height="40" width="10" y="30" x="15">
+                    <animate
+                        begin="-0.6s"
+                        values="1;0.2;1"
+                        keySplines="0.5 0 0.5 1;0.5 0 0.5 1"
+                        keyTimes="0;0.5;1"
+                        calcMode="spline"
+                        repeatCount="indefinite"
+                        dur="1s"
+                        attributeName="opacity"
+                    />
+                </rect>
+                <rect fill="#2dd1ff" height="40" width="10" y="30" x="35">
+                    <animate
+                        begin="-0.4s"
+                        values="1;0.2;1"
+                        keySplines="0.5 0 0.5 1;0.5 0 0.5 1"
+                        keyTimes="0;0.5;1"
+                        calcMode="spline"
+                        repeatCount="indefinite"
+                        dur="1s"
+                        attributeName="opacity"
+                    />
+                </rect>
+                <rect fill="#a9a9a9" height="40" width="10" y="30" x="55">
+                    <animate
+                        begin="-0.2s"
+                        values="1;0.2;1"
+                        keySplines="0.5 0 0.5 1;0.5 0 0.5 1"
+                        keyTimes="0;0.5;1"
+                        calcMode="spline"
+                        repeatCount="indefinite"
+                        dur="1s"
+                        attributeName="opacity"
+                    />
+                </rect>
+                <rect fill="#f9253f" height="40" width="10" y="30" x="75">
+                    <animate
+                        begin="-1s"
+                        values="1;0.2;1"
+                        keySplines="0.5 0 0.5 1;0.5 0 0.5 1"
+                        keyTimes="0;0.5;1"
+                        calcMode="spline"
+                        repeatCount="indefinite"
+                        dur="1s"
+                        attributeName="opacity"
+                    />
+                </rect>
+            </g>
+        </svg>
+    );
+}
--- src/components/races_comp/RacesComp.tsx
+++ src/components/races_comp/RacesComp.tsx
@@ -2,18 +2,24 @@ import "./RacesComp.css";
 import { useEffect, useState } from "react";
 import Page from "../../models/Page";
 import Race from "../../models/Race";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import RaceTicket from "./RaceTicket";
 import uaMonthNames from "../../ua_localize/uaMonthNames";
 import City from "../../models/City";
+import ReactPaginate, { ReactPaginateProps } from "react-paginate";
+import CustomLoading from "../CustomLoading";
 
 export default function RacesComp() {
     const [searchParams] = useSearchParams();
-    const location = useLocation();
-    
+    const navigate = useNavigate();
+
+    let defaultPage: Page<Race> = {count: 0, results: []}
+
+    const pageNum: string = searchParams.get("page") ?? "1";
+
     const city_from: string = searchParams.get("city_from") ?? "";
     const city_to: string = searchParams.get("city_to") ?? "";
-    
+
     const date_str: string = searchParams.get("date") ?? "";
     const date: Date = new Date(date_str);
     const time_from: Date = new Date(date);
@@ -22,11 +28,14 @@ export default function RacesComp() {
     time_to.setHours(23, 59, 59, 999);
     const passengers: string = searchParams.get("passengers") ?? "";
 
-    const [races, setRaces] = useState<Race[]>([]);
+    const [page, setPage] = useState<Page<Race>>(defaultPage);
     const [city1, setCity1] = useState<City>();
     const [city2, setCity2] = useState<City>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const fetchRaces = async () => {
+        setIsLoading(true);
+        setPage(defaultPage);
         try{
             const response: Response = await fetch(`${import.meta.env.VITE_API_SERVER}/races/?${new URLSearchParams({city_from, city_to, time_start_after: time_from.toISOString(), time_start_until: time_to.toISOString(), min_places: passengers})}`);
             if (!response.ok) {
@@ -34,10 +43,12 @@ export default function RacesComp() {
                 throw error;
             }
             const page: Page<Race> = await response.json();
-            const newRaces: Race[] = page.results;
-            setRaces(newRaces);
+            setPage(page);
+            defaultPage.count = page.count;
         } catch (error) {
             console.log(error);
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -69,7 +80,8 @@ export default function RacesComp() {
     useEffect(() => {
         fetchCities();
         fetchRaces();
-    }, [location]);
+        // console.log(GetAuthTokensFromLocalStorage())
+    }, [searchParams]);
 
     return (
         <div className="races-wrapper">
@@ -78,16 +90,48 @@ export default function RacesComp() {
                 <span className="races-direction">{city1?.name_ua} - {city2?.name_ua}</span>
                 <span className="races-note-2">Виїзд та прибуття за місцевим часом</span>
                 <span className="races-description">Розклад автобусів {city1?.name_ua} - {city2?.name_ua} на {date.getDate()} {uaMonthNames[date.getMonth()]}</span>
-                {city1 && city2 && races.map((race) => (
-                    <RaceTicket
-                        key={race.id}
-                        city_from={city1}
-                        city_to={city2}
-                        time_start={race.time_start}
-                        time_end={race.time_end}
-                        cost={race.cost}
+                {isLoading ? <>
+                    <CustomLoading />
+                </> : <>
+                    {city1 && city2 && page.results.map((race) => (
+                        <RaceTicket
+                            key={race.id}
+                            city_from={city1}
+                            city_to={city2}
+                            time_start={race.time_start}
+                            time_end={race.time_end}
+                            cost={race.cost}
+                        />
+                    ))}
+                    <ReactPaginate
+                        previousLabel="<"
+                        nextLabel=">"
+                        initialPage={Number(pageNum) - 1}
+                        onPageChange={(event) => {
+                            const newPageNum = event.selected + 1;
+                            navigate(`/races?${new URLSearchParams({city_from, city_to, date: date_str, passengers, page: newPageNum.toString()})}`);
+                        }}
+                        pageCount={Math.floor(page.count / 10)}
+                        containerClassName="pagination"
+                        activeClassName="pagination-active"
+                        previousClassName="pagination-prev"
+                        nextClassName="pagination-next"
+                        disabledClassName="pagination-disabled"
+                        renderOnZeroPageCount={(props: ReactPaginateProps) => (
+                            <ul className={props.containerClassName}>
+                                <li className={`${props.previousClassName} ${props.disabledClassName}`}>
+                                    <a>{props.previousLabel}</a>
+                                </li>
+                                <li className={props.activeClassName}>
+                                    <a>1</a>
+                                </li>
+                                <li className={`${props.nextClassName} ${props.disabledClassName}`}>
+                                    <a>{props.nextLabel}</a>
+                                </li>
+                            </ul>
+                        )}
                     />
-                ))}
+                </>}
             </div>
         </div>
     );
